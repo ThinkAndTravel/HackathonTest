e0ae465bf33e4a8429383f527a8053dc34858412
Slawik Iwanenko <good.worker.1997@gmail.com>
2018-01-22 16:50:06 +0200

    Add ViewModelBase

---
+++ BitCoin/BitCoin.Xam/BitCoin.Xam/Navigation/Services/INavigationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitCoin.Xam.Navigation.Services
+{
+    interface INavigationService
+    {
+
+    }
+}
---
+++ BitCoin/BitCoin.Xam/BitCoin.Xam/ViewModel/Base/ViewModelLocator.cs
@@ -0,0 +1,27 @@
+﻿using BitCoin.Xam.Navigation.Services;
+using Unity;
+
+namespace BitCoin.Xam.ViewModel.Base
+{
+    public class ViewModelLocator
+    {
+        readonly IUnityContainer _container;
+
+        public ViewModelLocator()
+        {
+            _container = new UnityContainer();
+
+            // ViewModels
+            _container.RegisterType<MainViewModel>();
+            
+            // Services     
+            _container.RegisterType<INavigationService, NavigationService>();
+        }
+        
+       
+        public MainViewModel MainViewModel {
+            get { return _container.Resolve<MainViewModel>(); }
+        }
+        
+    }
+}
