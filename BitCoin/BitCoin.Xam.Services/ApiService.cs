8559b298f923f31ba4c207f4f81e95cd3b0bc9a8
Max Osipyonok <perepishka@mail.ru>
2018-01-23 11:18:58 +0200

    Last24h service

--- BitCoin/BitCoin.Xam.Services/ApiService.cs
+++ BitCoin/BitCoin.Xam.Services/ApiService.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +11,30 @@ namespace BitCoin.Xam.Services
 {
     public static class ApiService
     {
+        /// <summary>
+        /// Returns  list of BidAskPair sorted by time field
+        /// or empry list, if some error occurs
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<List<BidAskPair>> Last24hPoints()
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Settings.App_Uri + @"Home/Last24hInfo");
+                HttpWebResponse response = (HttpWebResponse)(await request.GetResponseAsync());
 
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    var str = await reader.ReadToEndAsync();
+                    var list = JsonConvert.DeserializeObject<List<BidAskPair>>(str);
+                    list.Sort((a, b) => a.time.CompareTo(b.time));
+                    return list;
+                }
+            }catch(Exception e)
+            {
+                return new List<BidAskPair>();
+            }
+        }
     }
 }
---
+++ BitCoin/BitCoin.Xam.Services/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitCoin.Xam.Services
+{
+    public static class Settings
+    {
+        public static string App_Uri = @"http://localhost:60106/";
+    }
+}
